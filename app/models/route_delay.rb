dfcc9f1e4f43b504f16394e7121cda7d85f442f8
Dave Allie <dave@daveallie.com>
2016-02-23 07:01:54 +1000

    Restrict to just bus routes

--- app/models/route_delay.rb
+++ app/models/route_delay.rb
@@ -1,4 +1,10 @@
 class RouteDelay < ActiveRecord::Base
   include Filterable
   scope :route, -> (route_id) {where('"route_id" like ?', "#{route_id}%")}
+  scope :buses, -> {where("route_id ~* E'^[0-9]{3,4}(?:-[0-9]+)?$'")}
+
+  def self.bus_routes
+    routes = where("route_id ~* E'^[0-9]{3,4}(?:-[0-9]+)?$'").pluck('DISTINCT route_id')
+    routes.map{|r| r.split('-').first}
+  end
 end
